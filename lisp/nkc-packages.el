774d27eac52f8513f3b52e8c7b56e560973a1ac1
nat chapman <nat.k.chapman@gmail.com>
2015-12-07 22:47:45 -0600

    install packages with cask

---
+++ lisp/nkc-packages.el
@@ -0,0 +1,17 @@
+
+;; Dependencies
+
+;; [[file:~/.emacs.d/lisp/nkc-packages.org::*Dependencies][Dependencies:1]]
+
+(require 'use-package)
+(require 'bind-keys)
+
+;; Dependencies:1 ends here
+
+;; Provide
+
+;; [[file:~/.emacs.d/lisp/nkc-packages.org::*Provide][Provide:1]]
+
+(provide 'nkc-packages)
+
+;; Provide:1 ends here

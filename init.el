379847703bee05fc258ad669597851152c2b932d
nat chapman <nat.k.chapman@gmail.com>
2015-12-07 23:30:26 -0600

    started creating package configurations

--- init.el
+++ init.el
@@ -1,20 +1,17 @@
-
 ;; Load path
 ;;    I definitely could use use-package for this, but when there's bare
 ;;    files in an organizational directory it just feels easier to do
 ;;    without it. Plus there's a ton of stuff in the lisp/ directory that
 ;;    i'd rather not have to specify in use-package. On the other hand
 ;;    it's probably better for stuff from other people to be explicitly
-;;    labeled? So I might remove everything but lisp/ from this later.
+;;    labeled? So I might remove everything but lisp/ from this later. 
 
-;; [[file:~/.emacs.d/init.org::*Load%20path][Load\ path:1]]
-
+;; [[file:init.org::*Load%20path][Load\ path:1]]
 (eval-and-compile
   (mapc
    #'(lambda (path)
        (push (expand-file-name path user-emacs-directory) load-path))
    '("lib" "site-lisp" "lisp")))
-
 ;; Load\ path:1 ends here
 
 ;; TODO check for cask
@@ -23,8 +20,7 @@
 ;;    packages are actually installed, but we'll need to run
 ;;    package-initialize instead.
 
-;; [[file:~/.emacs.d/init.org::*check%20for%20cask][check\ for\ cask:1]]
-
+;; [[file:init.org::*check%20for%20cask][check\ for\ cask:1]]
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
@@ -32,7 +28,6 @@
 (pallet-mode t)
 (pallet-install)
 (pallet-update)
-
 ;; check\ for\ cask:1 ends here
 
 ;; use-package
@@ -43,20 +38,23 @@
 ;;    stuff goes with. At some point I'll look at customize, I
 ;;    promise. But not yet. So use-package, obv.
 
-;; [[file:~/.emacs.d/init.org::*use-package][use-package:1]]
-
+;; [[file:init.org::*use-package][use-package:1]]
 (eval-when-compile
   (setq use-package-verbose t)
   (require 'use-package))
-
 ;; use-package:1 ends here
+
+;; Packages
+;;    Setup for packages, package-specific bindings, that sort of thing.
+
+;; [[file:init.org::*Packages][Packages:1]]
+(use-package nkc-packages)
+;; Packages:1 ends here
 
 ;; Settings
 ;;    Basically anything that doesn't fit into an above category. Things
 ;;    that would be in a use-package :config if emacs were a package.
 
-;; [[file:~/.emacs.d/init.org::*Settings][Settings:1]]
-
+;; [[file:init.org::*Settings][Settings:1]]
 (use-package nkc-settings)
-
 ;; Settings:1 ends here
--- lisp/nkc-packages.el
+++ lisp/nkc-packages.el
@@ -1,17 +1,28 @@
-
 ;; Dependencies
 
-;; [[file:~/.emacs.d/lisp/nkc-packages.org::*Dependencies][Dependencies:1]]
-
+;; [[file:nkc-packages.org::*Dependencies][Dependencies:1]]
 (require 'use-package)
-(require 'bind-keys)
-
+(require 'bind-key)
 ;; Dependencies:1 ends here
+
+;; Magit
+;;    [[https://github.com/magit/magit][github]] [[http://magit.vc/manual/][manual]]
+
+;; [[file:nkc-packages.org::*Magit][Magit:1]]
+(use-package magit
+  :bind (("C-c g s" . magit-status)
+         ("C-c g p" . magit-pull)))
+;; Magit:1 ends here
+
+;; Solarized
+;;    [[https://github.com/sellout/emacs-color-theme-solarized][github]]
+
+;; [[file:nkc-packages.org::*Solarized][Solarized:1]]
+(load-theme 'solarized t)
+;; Solarized:1 ends here
 
 ;; Provide
 
-;; [[file:~/.emacs.d/lisp/nkc-packages.org::*Provide][Provide:1]]
-
+;; [[file:nkc-packages.org::*Provide][Provide:1]]
 (provide 'nkc-packages)
-
 ;; Provide:1 ends here

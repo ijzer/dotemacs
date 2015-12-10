b5826983a1085a6ed8d5676234e111632018ccd6
nat chapman <nat.k.chapman@gmail.com>
2015-12-10 02:12:10 -0600

    change default font on w32 to meslo

--- lisp/nkc-settings.el
+++ lisp/nkc-settings.el
@@ -1,43 +1,45 @@
-
-;; UI Changes
+;; GUI
 ;;    The mouse is the devil in emacs. Someday I'll figure out how to get
 ;;    rid of the damn thing entirely. It'll be beautiful, lemme tell you.
 
-;; [[file:~/.emacs.d/lisp/nkc-settings.org::*UI%20Changes][UI\ Changes:1]]
-
+;; [[file:nkc-settings.org::*GUI][GUI:1]]
 (tooltip-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (setq inhibit-startup-screen t
       use-dialog-box nil)
+;; GUI:1 ends here
 
-;; UI\ Changes:1 ends here
+;; Font
+
+;; [[file:nkc-settings.org::*Font][Font:1]]
+(if (eq window-system 'w32)
+    (add-to-list 'default-frame-alist
+                 '(font . "Meslo LG S-10")))
+;; Font:1 ends here
 
 ;; Backups
 ;;    emacs likes to strew backup and autosave files everywhere. They
 ;;    ought to be in one place. So let's do that.
 
-;; [[file:~/.emacs.d/lisp/nkc-settings.org::*Backups][Backups:1]]
-
+;; [[file:nkc-settings.org::*Backups][Backups:1]]
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
       auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory
                                                       "auto-save-list") t)))
-
 ;; Backups:1 ends here
 
+
 ;; I habitually use version control so I don't need many backups and I
-;;    don't care if I don't have old ones around.
+;; don't care if I don't have old ones around.
 
-;; [[file:~/.emacs.d/lisp/nkc-settings.org::*Backups][Backups:1]]
-
+;; [[file:nkc-settings.org::*Backups][Backups:1]]
 (setq delete-old-versions t
       kept-old-versions 0
       kept-new-versions 5
       version-control t
       vc-make-backup-files t
       backup-by-copying t)
-
 ;; Backups:1 ends here
 
 ;; Mode line

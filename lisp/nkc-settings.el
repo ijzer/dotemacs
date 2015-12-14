62a622d4bf840b7afcb563ee7ec3c98e79e0ff57
nat chapman <nat.k.chapman@gmail.com>
2015-12-14 07:39:36 -0600

    added fallback fonts for unicode symbols

--- lisp/nkc-settings.el
+++ lisp/nkc-settings.el
@@ -11,13 +11,94 @@
       use-dialog-box nil)
 ;; GUI:1 ends here
 
-;; Font
+;; Backup
+;;      Source Code Pro
+;;      [[http://adobe-fonts.github.io/source-code-pro/]]
 
-;; [[file:nkc-settings.org::*Font][Font:1]]
-(if (eq window-system 'w32)
-    (add-to-list 'default-frame-alist
-                 '(font . "Meslo LG S-10")))
-;; Font:1 ends here
+;; [[file:nkc-settings.org::*Backup][Backup:1]]
+(set-fontset-font "fontset-default"
+                  'unicode
+                  (font-spec :name "Source Code Pro" :size 13
+                             :weight 'bold :width 'normal))
+;; Backup:1 ends here
+
+;; Default
+;;      Meslo LG S
+;;      [[https://github.com/andreberg/Meslo-Font]] 
+
+;; [[file:nkc-settings.org::*Default][Default:1]]
+(set-fontset-font "fontset-default"
+                  'ascii
+                  (font-spec :name "Meslo LG S"
+                             :weight 'normal :width 'normal)
+                  nil 'prepend)
+;; Default:1 ends here
+
+;; [[https://en.wikipedia.org/wiki/Letterlike_Symbols][Unicode Letterlike Symbols]] 
+
+;; [[file:nkc-settings.org::*%5B%5Bhttps:/en.wikipedia.org/wiki/Letterlike_Symbols%5D%5BUnicode%20Letterlike%20Symbols%5D%5D][\[\[https://en\.wikipedia\.org/wiki/Letterlike_Symbols\]\[Unicode\ Letterlike\ Symbols\]\]:1]]
+(set-fontset-font "fontset-default"
+                  '(#x2100 . #x214F)
+                  (font-spec :name "Lucida Sans Unicode"
+                             :weight 'normal :width 'normal) nil
+                  'prepend)
+;; \[\[https://en\.wikipedia\.org/wiki/Letterlike_Symbols\]\[Unicode\ Letterlike\ Symbols\]\]:1 ends here
+
+;; [[https://en.wikipedia.org/wiki/Arrows_(Unicode_block)][Unicode Arrows]] 
+
+;; [[file:nkc-settings.org::*%5B%5Bhttps:/en.wikipedia.org/wiki/Arrows_(Unicode_block)%5D%5BUnicode%20Arrows%5D%5D][\[\[https://en\.wikipedia\.org/wiki/Arrows_\(Unicode_block\)\]\[Unicode\ Arrows\]\]:1]]
+(set-fontset-font "fontset-default"
+                  '(#x2190 . #x21FF)
+                  (font-spec :name "Lucida Sans Unicode"
+                             :weight 'normal :width 'normal) nil
+                             'prepend)
+;; \[\[https://en\.wikipedia\.org/wiki/Arrows_\(Unicode_block\)\]\[Unicode\ Arrows\]\]:1 ends here
+
+;; [[https://en.wikipedia.org/wiki/Mathematical_Operators][Unicode Math]] 
+
+;; [[file:nkc-settings.org::*%5B%5Bhttps:/en.wikipedia.org/wiki/Mathematical_Operators%5D%5BUnicode%20Math%5D%5D][\[\[https://en\.wikipedia\.org/wiki/Mathematical_Operators\]\[Unicode\ Math\]\]:1]]
+(set-fontset-font "fontset-default"
+                    '(#x2200 . #x22FF)
+                    (font-spec :name "NanumGothic"
+                               :weight 'normal :width 'normal) nil
+                               'prepend)
+;; \[\[https://en\.wikipedia\.org/wiki/Mathematical_Operators\]\[Unicode\ Math\]\]:1 ends here
+
+;; [[https://en.wikipedia.org/wiki/Box-drawing_character][Unicode Line Drawing]] 
+
+;; [[file:nkc-settings.org::*%5B%5Bhttps:/en.wikipedia.org/wiki/Box-drawing_character%5D%5BUnicode%20Line%20Drawing%5D%5D][\[\[https://en\.wikipedia\.org/wiki/Box-drawing_character\]\[Unicode\ Line\ Drawing\]\]:1]]
+(set-fontset-font "fontset-default"
+                    '(#x2500 . #x257F)
+                    (font-spec :name "Lucida Sans Unicode"
+                               :weight 'normal :width 'normal) nil
+                               'prepend)
+;; \[\[https://en\.wikipedia\.org/wiki/Box-drawing_character\]\[Unicode\ Line\ Drawing\]\]:1 ends here
+
+;; [[https://en.wikipedia.org/wiki/Enclosed_Alphanumerics][Unicode Circled Alnum]] 
+
+;; [[file:nkc-settings.org::*%5B%5Bhttps:/en.wikipedia.org/wiki/Enclosed_Alphanumerics%5D%5BUnicode%20Circled%20Alnum%5D%5D][\[\[https://en\.wikipedia\.org/wiki/Enclosed_Alphanumerics\]\[Unicode\ Circled\ Alnum\]\]:1]]
+(set-fontset-font "fontset-default"
+                    '(#x2460 . #x24FF)
+                    (font-spec :name "NanumGothic"
+                               :weight 'normal :width 'normal) nil
+                               'prepend)
+;; \[\[https://en\.wikipedia\.org/wiki/Enclosed_Alphanumerics\]\[Unicode\ Circled\ Alnum\]\]:1 ends here
+
+;; [[https://en.wikipedia.org/wiki/Dingbat][Unicode Dingbats]] 
+
+;; [[file:nkc-settings.org::*%5B%5Bhttps:/en.wikipedia.org/wiki/Dingbat%5D%5BUnicode%20Dingbats%5D%5D][\[\[https://en\.wikipedia\.org/wiki/Dingbat\]\[Unicode\ Dingbats\]\]:1]]
+(set-fontset-font "fontset-default"
+                    '(#x2700 . #x27BF)
+                    (font-spec :name "NanumGothic"
+                               :weight 'normal :width 'normal) nil
+                    'prepend)
+;; \[\[https://en\.wikipedia\.org/wiki/Dingbat\]\[Unicode\ Dingbats\]\]:1 ends here
+
+;; Install fonts
+
+;; [[file:nkc-settings.org::*Install%20fonts][Install\ fonts:1]]
+(set-frame-font "fontset-default")
+;; Install\ fonts:1 ends here
 
 ;; Backups
 ;;    emacs likes to strew backup and autosave files everywhere. They
@@ -26,7 +107,7 @@
 ;; [[file:nkc-settings.org::*Backups][Backups:1]]
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
       auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory
-                                                      "auto-save-list") t)))
+                                                      "auto-save-list/") t)))
 ;; Backups:1 ends here
 
 
@@ -61,7 +142,7 @@
 
 ;; [[file:nkc-settings.org::*autofill][autofill:1]]
 (setq-default fill-column 75
-	      auto-fill-function 'do-auto-fill)
+              auto-fill-function 'do-auto-fill)
 ;; autofill:1 ends here
 
 ;; Visual line mode
